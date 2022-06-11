cd522f6f74aae0e7a3ba8407d251bf85eb96c896
valderyjr <valderyjunior2002@gmail.com>
2022-06-11 16:00:37 -0300

    criando estrutura de rotas e pagina home

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Header from './components/Header'
+import Home from './pages/Home'
 
 function App() {
 	return (
-		<>
-			<Header />
-		</>
+		<BrowserRouter>
+				<Header/>
+			<Routes>
+				<Route path='/' element={<Home />}/>
+			</Routes>
+		</BrowserRouter>
 	)
 }
 
---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import style from './Home.module.scss'
+import FilterButton from '../../components/FilterButton'
+
+function Home() {
+	return (
+		<main className={style.principal}>
+			<header className={style.principal__header}>
+				<div className={style.header}>
+					<h2>Dashboard</h2>
+					<h3>Desafio técnico front-end</h3>
+				</div>
+				<FilterButton />
+			</header>
+			<section className={style.principal__graficos}>
+				<section className={style.grafico}>Aqui ficarão os gráficos</section>
+				<section className={style.grafico}>Aqui ficarão os gráficos</section>
+			</section>
+		</main>
+	)
+}
+
+export default Home
